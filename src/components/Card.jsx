4f460426e7818a837c6720a3780b94de5c677a4b
Victor Kist <victoraugustokist@gmail.com>
2022-04-07 21:37:07 -0300

    ajuste no card de repos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,22 @@
-function Card({ title }) {
+function Card({ title, description, topics, url, language, stars }) {
 
 	return(
-    <li className="w-full">
-      <h2>{title}</h2>
+    <li className="flex flex-col items-start  w-full pb-6 mb-6 border-b border-gray-600 border-solid">
+
+      <a className="text-xl text-blue-500 hover:underline" href={url}>
+        <h2 >{title}</h2>
+      </a>
+      <p className="mb-2 text-sm text-left leading-tight text-gray-400">{description}</p>
+
+      <ul className="flex flex-row gap-2 mb-3">
+        {topics.slice(0, 5).map((item, i) => <li key={i} className="px-2 text-xs bg-gray-600 text-gray-50 rounded-full">{item}</li>)}
+      </ul>
+
+      <div className="flex flex-row gap-2 text-sm text-left leading-tight text-gray-400">
+        <span>{language}</span>
+        <span>✰{stars}</span>
+      </div>
+
     </li>
 	)
 }
--- src/view/home/Content.jsx
+++ src/view/home/Content.jsx
@@ -16,21 +16,21 @@ function Content({ repos, stars }) {
   }
 
 	return(
-    <div className='flex flex-col items-center justify-between w-full md:w-7/12 lg:w-9/12 h-full -mt-6'>
+    <div className='flex flex-col items-center justify-between w-full md:w-7/12 lg:w-9/12 h-full mt-10 md:-mt-6'>
 
       <div className='flex flex-row items-center gap-5 mb-10 text-gray-50'>
         <button
           onClick={() => setOption('repos')}
-          className='hidden md:flex hover:scale-110 transition'
+          className='hover:scale-110 transition'
           >Repositories</button>
         <button
           onClick={() => setOption('stars')}
-          className='hidden md:flex hover:scale-110 transition'
+          className='hover:scale-110 transition'
           >Stars</button>
       </div>
 
-      <ul>
-        {content().map((item, i) => <Card key={i} title={item.full_name} />)}
+      <ul className='flex flex-col w-full'>
+        {content().map((item, i) => <Card key={i} title={item.full_name} description={item.description} topics={item.topics} url={item.html_url} language={item.language} stars={item.stargazers_count} />)}
       </ul>
 
     </div>
